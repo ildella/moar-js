cdc9210168334b5bba1cd5b36fc24129597f1fa1
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-07-26 14:52:09 +0200

    now env config return the macaroon in utf 8, ready for the http clients

--- lnd/lnd-env-config.js
+++ lnd/lnd-env-config.js
@@ -3,13 +3,11 @@ const {curry} = require('../fusto')
 module.exports = curry((future, {
   host = '127.0.0.1',
   port = 8080,
-  // username,
   macaroonType = 'readonly',
 }) => {
-  // const macaroonPath = `${basePath}/${username}/data/chain/bitcoin/regtest/admin.macaroon`
   const baseUrl = `${host}:${port}`
-  // const macaroon = process.env[`LND_${macaroonType.toUpperCase()}_MACAROON`]
-  const macaroon = process.env.LND_INVOICE_MACAROON
-  const cert = process.env.LND_TLS_CERT
+  const macaroon = process.env[`LND_${macaroonType.toUpperCase()}_MACAROON`]
+  const hexCertificate = process.env.LND_TLS_CERT
+  const cert = Buffer.from(hexCertificate, 'hex').toString('utf8')
   return {macaroon, cert, baseUrl}
 })
