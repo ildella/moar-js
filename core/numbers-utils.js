9e5340b5c73d0783a5b8150aadd0c8265b35455b
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-08-20 14:58:43 +0200

    number utility will just return the input if is a number already

--- core/numbers-utils.js
+++ core/numbers-utils.js
@@ -1,4 +1,5 @@
 const number = text => {
+  if (typeof text === 'number') return text
   if (typeof text !== 'string') throw new Error('input must be a string')
   if (text.length === 0) throw new Error('empty string not valid')
   const n = Number(text)
@@ -8,7 +9,7 @@ const number = text => {
 
 const positive = text => {
   const n = number(text)
-  if (n < 0) throw new Error('negative')
+  if (n < 0) throw new Error(`${text} is negative`)
   return n
 }
 
