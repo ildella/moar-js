072a46889eda621bfc8ecbda29d43b1b3e0b55d6
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-09-17 10:01:14 +0000

    ignore lnd tests on CI

---
+++ jest.ci.config.js
@@ -0,0 +1,16 @@
+/*
+  Documentation:
+    https://jestjs.io/docs/configuration#projects-arraystring--projectconfig
+*/
+
+const {preset} = require('./jest')
+
+module.exports = {
+  ...preset(),
+  projects: [
+    '<rootDir>/tests/axios',
+    '<rootDir>/tests/core',
+    '<rootDir>/tests/fusto',
+    '<rootDir>/tests/http',
+  ],
+}
