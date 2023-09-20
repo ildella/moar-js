deb2c50f565a813310f5f1bba8c983272aaffd4a
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-09-20 08:00:26 +0000

    just trace errors that are 4xx

--- http/http-error-handler.js
+++ http/http-error-handler.js
@@ -9,7 +9,7 @@ reply) => {
   })
   const code = error.status || error.statusCode || 500
   const toPrint = verbose === true ? error : {message: error.message}
-  if (code < 500) log.warn({id, ip}, toPrint)
+  if (code < 500) log.trace({id, ip}, toPrint)
   if (code >= 500) log.error({id, ip}, toPrint)
   return reply.code(code).send(toPrint)
 }
