12ccfd7580dd9ad4f12f9517625e48628965c1e8
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-09-28 08:55:35 +0000

    apparently the old method was the right one

--- http/tracer-fastify-logger.js
+++ http/tracer-fastify-logger.js
@@ -3,7 +3,9 @@ const {local} = require('../tracer')
 module.exports = ({app, logLevel}) => {
   const log = local({level: logLevel})
   app.log = log
-  app.decorateRequest('log', log)
-  // app.decorate('log', log)
+  app.addHook('onRequest', (request, reply, done) => {
+    request.log = log
+    done()
+  })
   return log
 }
