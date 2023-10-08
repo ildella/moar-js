6165660aa6fdf3eb5e87a11b80fee52032ef96e8
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-10-08 19:20:49 +0000

    removed log from the app object, added decorateRequest which seem to be the right way to do this in fastify... + bump deps

--- http/tracer-fastify-logger.js
+++ http/tracer-fastify-logger.js
@@ -3,6 +3,7 @@ const {local} = require('../tracer')
 module.exports = ({app, logLevel}) => {
   const log = local({level: logLevel})
   // app.log = log
+  app.decorateRequest('userId', null)
   app.addHook('onRequest', (request, reply, done) => {
     request.log = log
     done()
