907abb97bcab4261c41eb78d15a54a176598e7f1
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-11-04 14:20:31 +0000

    safe multiply

--- core/math.js
+++ core/math.js
@@ -2,4 +2,9 @@ const curry = require('just-curry-it')
 
 const add = (decimals, a, b) => Number((a + b).toFixed(decimals))
 
-module.exports = {add: curry(add)}
+const multiply = (decimals, a, b) => Number((a * b).toFixed(decimals))
+
+module.exports = {
+  add: curry(add),
+  multiply: curry(multiply),
+}
--- tests/core/math.test.js
+++ tests/core/math.test.js
@@ -1,9 +1,15 @@
-const {add} = require('../../core/math')
+const {add, multiply} = require('../../core/math')
 
-const fixed2 = add(2)
+const add2 = add(2)
+const multiply2 = multiply(2)
 
 test('add', () => {
-  expect(fixed2(2, 3)).toEqual(5)
-  expect(fixed2(2.555, 3.111)).toEqual(5.67)
+  expect(add2(2, 3)).toEqual(5)
+  expect(add2(2.555, 3.111)).toEqual(5.67)
   expect(add(3, 2.555, -3.111)).toEqual(-0.556)
+})
+
+test('multiply', () => {
+  expect(multiply2(3, 3)).toEqual(9)
+  expect(multiply2(3.4, 3.84)).toEqual(13.06)
 })
