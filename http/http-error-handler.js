ad0bbb6615f1388a996d4091751a473b6eebd3e5
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-11-14 02:54:14 +0000

    more info in the error warnings and error, removed useless debug.

--- http/http-error-handler.js
+++ http/http-error-handler.js
@@ -1,16 +1,17 @@
 // eslint-disable-next-line complexity
 const errorHandler = ({verbose = true} = {}) => (error, {
   log,
-  id, ip, hostname, originalUrl, url, body,
+  id, ip, hostname, originalUrl, url, body, method,
 },
 reply) => {
-  log.debug({
-    id, ip, hostname, originalUrl, url, body,
-  })
   const code = error.status || error.statusCode || 500
   const toPrint = verbose === true ? error : {message: error.message}
-  if (code < 500) log.trace({id, ip}, toPrint)
-  if (code >= 500) log.error({id, ip}, toPrint)
+  if (code < 500) log.warn({
+    id, ip, hostname, originalUrl, url, body, method,
+  }, toPrint)
+  if (code >= 500) log.error({
+    id, ip, hostname, originalUrl, url, body, method,
+  }, toPrint)
   return reply.code(code).send(toPrint)
 }
 
