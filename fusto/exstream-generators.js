5f2e9d988a2251e924af1d30e0a07f0c5580f5b9
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2023-11-26 15:50:32 +0000

    resolution on this babel package used by jest that has a critical security issue

--- fusto/exstream-generators.js
+++ fusto/exstream-generators.js
@@ -5,14 +5,12 @@ const arrayMapping = ({
   prefix = '',
   items,
 }) => (push, next) => {
-  // console.log({items})
   items.map((item, index) => {
     // console.log({prefix, item, index})
     // const index = zeroBased ? i : i + 1
     push({item: `${prefix}${item}`, index})
     next()
   })
-  // console.log('DONE.')
   push(__.nil)
 }
 
